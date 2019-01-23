824f743d4ae93c0e239534d1b60d1f49c8a91c30
Parsa Enami <parsaenami@aut.ac.ir>
2019-01-23 23:20:43 +0330

    saving the output

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 import random as r
-import numpy as np
+
 import cv2 as cv
-from matplotlib import pyplot as plt
+import numpy as np
 
 index = []
+
+# cap = cv.VideoCapture("C:\\Users\\Parsa\\PycharmProjects\\ai_project_3\\1.mp4")
+cap = cv.VideoCapture(0)
+fgbg = cv.createBackgroundSubtractorKNN(history=5000)
+
+img = cv.imread("C:\\Users\\Parsa\OneDrive\\university\\semester 5\\AI\\FinalProject\\drop.png")
+img = cv.resize(img, (7, 7))
 
 
 def place_image(start, end, frame):
     print(img[0, 0])
+
     for x in range(img.shape[0]):
         for y in range(img.shape[1]):
             if (img[x, y] != np.array([0, 0, 0])).all():
@@ -18,26 +26,23 @@ def drop(mask, film):
     new_frame = film.copy()
     for d in range(3):
         index.append([0, r.randrange(new_frame.shape[1] - img.shape[1])])
+
     for t in index:
         if t[0] >= (mask.shape[0] - 30):
             index.remove(t)
+
     for i in range(len(index)):
         s, x, e, y = index[i][0], img.shape[0], index[i][1], img.shape[1]
+
         if (mask[s - x + 6, e - y + 4] == np.array([0, 0, 0])).all():
             if s <= mask.shape[0] - 30:
                 index[i] = [s + 30, y]
             place_image(s - x, e - y, new_frame)
+
         else:
             place_image(s - x, e - y, new_frame)
     return new_frame
 
-
-# cap = cv.VideoCapture("C:\\Users\\Parsa\\PycharmProjects\\ai_project_3\\1.mp4")
-cap = cv.VideoCapture(0)
-fgbg = cv.createBackgroundSubtractorKNN(history=5000)
-
-img = cv.imread("C:\\Users\\Parsa\OneDrive\\university\\semester 5\\AI\\FinalProject\\drop.png")
-img = cv.resize(img, (7, 7))
 
 fourcc = cv.VideoWriter_fourcc(*'XVID')
 _, frame0 = cap.read()
@@ -48,6 +53,7 @@ while (True):
 
     ret, frame = cap.read()
     fgmask = fgbg.apply(frame, learningRate=0.02)
+
     morph = fgmask.copy()
     kernel = cv.getStructuringElement(cv.MORPH_RECT, (4, 2))
     morph = cv.morphologyEx(morph, cv.MORPH_CLOSE, kernel)
@@ -59,9 +65,11 @@ while (True):
     if ret:
         out.write(f)
 
-    cv.imshow('frame00', morph)
-    cv.imshow('frame', f)
+    cv.imshow('background and foreground', morph)
+    cv.imshow('main frame', f)
+
     k = cv.waitKey(1) & 0xff
+
     if k == 27:
         break
 
