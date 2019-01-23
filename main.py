ddf34d31dead37b75109c6a4c636a38ee274695b
Parsa Enami <parsaenami@aut.ac.ir>
2019-01-23 23:10:40 +0330

    changes

--- main.py
+++ main.py
@@ -3,33 +3,39 @@ import numpy as np
 import cv2 as cv
 from matplotlib import pyplot as plt
 
+index = []
 
-
-def place_image(s, e, backi):
+def place_image(start, end, frame):
     print(img[0, 0])
     for x in range(img.shape[0]):
         for y in range(img.shape[1]):
             if (img[x, y] != np.array([0, 0, 0])).all():
-                backi[s + x, e + y] = img[x, y]
+                frame[start + x, end + y] = img[x, y]
 
 
 def drop(mask, film):
     new_frame = film.copy()
-    # place_image(0 + k1, t, film)
-
-
-    # if k1 > 440:
-    #     k1 = 0
-    # k1 += 4
+    for d in range(3):
+        index.append([0, r.randrange(new_frame.shape[1] - img.shape[1])])
+    for t in index:
+        if t[0] >= (mask.shape[0] - 30):
+            index.remove(t)
+    for i in range(len(index)):
+        s, x, e, y = index[i][0], img.shape[0], index[i][1], img.shape[1]
+        if (mask[s - x + 6, e - y + 4] == np.array([0, 0, 0])).all():
+            if s <= mask.shape[0] - 30:
+                index[i] = [s + 30, y]
+            place_image(s - x, e - y, new_frame)
+        else:
+            place_image(s - x, e - y, new_frame)
+    return new_frame
 
 
 cap = cv.VideoCapture(0)
 fgbg = cv.createBackgroundSubtractorMOG2()
-# for i in fgbg:
-#     print(i)
 
 img = cv.imread("C:\\Users\\Parsa\OneDrive\\university\\semester 5\\AI\\FinalProject\\drop.png")
-img = cv.resize(img, (25, 25))
+img = cv.resize(img, (7, 7))
 
 k1 = 0
 
@@ -40,39 +46,14 @@ while (True):
     ret, frame = cap.read()
     bg_learning_rate = 0
     fgmask = fgbg.apply(frame, learningRate=0.2)
-    # print(type(fgbg))
+
     success, film = cap.read()
-
-
-
-    # for d in range(1):
-    #     execution.append(r.randrange(film.shape[1] // 2))
-        # k1 = 0
-
-
-    # print(type(film))
-
-    # for x in img:
-    #     for y in img:
-    #         # print(x, y)
-    #         film[x, y] = img[x, y]
-
-    # for t in execution:
-    #     drop(t, film, img, k1)
-
-
-
-    ###place_image(k1, 0, film, img)
 
     cv.imshow('frame00', fgmask)
     cv.imshow('frame', film)
     k = cv.waitKey(1) & 0xff
     if k == 27:
         break
-
-    # if k1 > 440:
-    #     k1 = 0
-    # k1 += 4
 
 
 
